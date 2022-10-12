c36002163a1009c7e2f83316b9c464121387acc7
Guilherme <augusto.g@outlook.com.br>
2022-10-12 14:09:01 -0300

    Criacao de usuario

--- bancodedados.py
+++ bancodedados.py
@@ -2,21 +2,46 @@ import mysql.connector
 
 con = mysql.connector.connect(host='localhost', database='sistemaponto', user='root', password='')
 
-if con.is_connected():
-    db_info = con.get_server_info()
-    print(f"Conectado ao servidor {db_info}")
-    cursor = con.cursor()
-    cursor.execute("select database ();")
-    linha = cursor.fetchone()
-    print(f"Conectado ao bando de dados {linha}")
+
+class database:
+    
+    def conectar_db():
+        if con.is_connected():
+            db_info = con.get_server_info()
+            print(f"Conectado ao servidor {db_info}")
+            cursor = con.cursor()
+            cursor.execute("select database ();")
+            linha = cursor.fetchone()
+            print(f"Conectado ao bando de dados {linha}")
+
+    def desconectar_db():
+        if con.is_connected():
+            cursor = con.cursor()
+            cursor.close()
+            con.close()
+            print("Conexcao com servidor encerrada")
 
 
+ 
+class usuario:
 
+    def __init__(self, nome, cpf):
+        self.nome = nome
+        self.cpf = cpf
+        pass
+ 
 
-def desconectar_db():
-    if con.is_connected():
-        cursor.close()
-        con.close()
-        print("Conexcao com servidor encerrada")
+    def criar_usuario(nome, cpf):
+        nome = nome
+        cpf = cpf
 
+        database.conectar_db()
 
+        declaracao = f"""insert into usuario values (id_usuario, '{nome}', '{cpf}');"""
+        inserir_sql = declaracao
+        print(inserir_sql)
+
+        cursor = con.cursor()
+        cursor.execute(inserir_sql)
+        con.commit
+        database.desconectar_db()
--- sistema.py
+++ sistema.py
@@ -1,18 +1,24 @@
 from datetime import datetime
 
+from bancodedados import database, usuario
+
 
 def menu():
     print("""Olá, bem vindo ao sistema de ponto eletronico.
     Digite [1] para dar entrada no sistema.
     Digite [2] para dar saída no sistema.
     Digite [3] para criar um novo usuário.
-    Digite [4] para excluir um novo usuário.
+    Digite [4] para excluir um usuário.
     Digite [5] para consultar o histórico de algum usuário.
     """)
     while True:
         resposta = input("Digite aqui sua opcão: ")
         if resposta == '1':
             entrada()
+        
+        if resposta =='3':
+            criar_usuario()
+
         elif resposta == '99':
             print("finalizando aplicacao")
             break
@@ -36,5 +42,30 @@ def saida():
     hora_correta = hora.strftime('%H:%M %d/%m/%Y')
     print(f"Saída com sucesso: {hora_correta}")
 
+def criar_usuario():
+    resposta = input("Deseja criar um usuario? Digite [1] para continuar: ")
+    while resposta == '1':
+        nome = str(input("Digite o nome do usuario: "))
+        cpf = int(input("Digite o numero de CPF do usuario: "))
+        print(nome, cpf)
+
+        try: 
+            usuario.criar_usuario(nome, cpf)
+            print("Usuario criado com sucesso")
+
+        except:
+            print("Houve algum erro, tente novamente")
+
+        resposta = (input("Deseja adicionar mais um usuário? Se sim digite [1]. Caso não deseje digite [0]: "))
+        if resposta == '0':
+            menu()
+        elif resposta != '1':
+            resposta = input("Valor desejado inválido, por favor digitar novamente: ")
+
 
 menu()
+
+        
+
+
+
